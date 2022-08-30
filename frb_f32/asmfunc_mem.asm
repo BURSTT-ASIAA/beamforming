bdd9cbc88c98377690b3d29c5dd268ec771b039e
Chih-Yi Wen <wency@asiaa.sinica.edu.tw>
2022-08-30 12:55:02 +0800

    use a better matrix multiplication method

---
+++ frb_f32/asmfunc_mem.asm
@@ -0,0 +1,86 @@
+    section .data
+
+bitMask:    db  16 dup 0xF0
+
+    section .bss
+
+align       64
+realV:      resz 1
+imagV:      resz 1
+
+    section .text
+
+    global asmfunc
+asmfunc:
+    push rbx
+    push r15
+    push rbp
+    mov rbp, rsp
+
+    ; load xmm15, xmm8
+    vmovdqu8 xmm15, [bitMask]
+
+    ; zeroed zmm13(sum for square)
+    vxorps zmm13, zmm13, zmm13
+    xor r15, r15
+
+integralLp:
+    ; unpack antenna data to realV(high 4 bits), imagV(low)
+    vmovdqu8 xmm0, [rsi+r15]
+    vmovdqu8 xmm1, xmm0
+
+    pand xmm0, xmm15
+    vpmovsxbd zmm0, xmm0
+    vpsrad zmm0, zmm0, 4
+    vcvtdq2ps zmm0, zmm0
+    vmovaps [realV], zmm0
+
+    psllw xmm1, 4
+    pand xmm1, xmm15
+    vpmovsxbd zmm1, xmm1
+    vpsrad zmm1, zmm1, 4
+    vcvtdq2ps zmm1, zmm1
+    vmovaps [imagV], zmm1
+
+    ; clear zmm11 and zmm12 for real and imag parts
+    xor rax, rax
+    mov rbx, rdi
+    vxorps zmm11, zmm11, zmm11
+    vxorps zmm12, zmm12, zmm12
+rowLp:
+    ; load a column from matrix
+    vmovaps zmm2, [rbx]
+    vmovaps zmm3, [rbx+64]
+
+    ; load a vector element
+    vbroadcastss zmm0, [realV+rax*4]
+    vbroadcastss zmm1, [imagV+rax*4]
+
+    ; calculate real part
+    vfmadd231ps zmm11, zmm0, zmm2
+    vfnmadd231ps zmm11, zmm1, zmm3
+
+    ; calculate imag part
+    vfmadd231ps zmm12, zmm0, zmm3
+    vfmadd231ps zmm12, zmm1, zmm2
+
+    add rbx, 128
+    inc rax
+    cmp rax, 16
+    jb rowLp
+
+    ; integral
+    vfmadd231ps zmm13, zmm11, zmm11
+    vfmadd231ps zmm13, zmm12, zmm12
+
+    add r15, 16
+    dec rdx
+    jnz integralLp
+
+    ; copy to destination
+    vmovaps [rcx], zmm13
+
+    leave
+    pop r15
+    pop rbx
+    ret
---
+++ frb_f32/testasm.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define nvec 40000000
+
+int asmfunc(float *mat, char *vec, long n, float *dest);
+
+int main(void) {
+    float dest[16] __attribute__ ((aligned (64)));
+    float mat[16*16*2];
+    char *vec;
+    int i, j, n, p, p2;
+    clock_t start, end;
+    double cpu_time_used;
+
+    for (j=0; j<16; j++) {
+        for (i=0; i<16; i++) {
+            p = i * 32 + j;
+            p2 = i * 32 + j + 16;
+            if (i == j) {
+                mat[p] = 1;
+                mat[p2] = 1;
+            } else {
+                mat[p] = 0;
+                mat[p2] = 0;
+            }
+        }
+    }
+
+    /* random values (or 0?) */
+    vec = malloc(16*nvec);
+
+    for (i=0; i<10; i++) {
+        start = clock();
+        asmfunc(mat, vec, nvec, dest);
+        end = clock();
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        printf("Datasize: %d x 16 bytes  CPU time : %.5f\n", nvec, cpu_time_used);
+    }
+
+    free(vec);
+    return 0;
+}
