0d4476c3498ef16c99000687ec937c2a058e4ed0
Chih-Yi Wen <wency@asiaa.sinica.edu.tw>
2022-10-21 14:28:43 +0800

    integration time is now a parameter

--- mc_i16_v5/check_timing.c
+++ mc_i16_v5/check_timing.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
+#include <float.h>
 
 #include <rte_memory.h>
 #include <rte_launch.h>
@@ -16,7 +17,10 @@
 #include <rte_malloc.h>
 
 #define NR_pack 400*1000UL // 6.4G
+#define NR_sum 400UL  // integral count
+#define NR_run 1000UL // NR_pack / NR_sum
 #define NR_ch 64UL
+#define NR_cpu 16 // 1024 / NR_ch
 
 typedef struct {
 	short *mat;
@@ -24,6 +28,7 @@ typedef struct {
 	long nr;
 	long nc;
 	float *dest;
+	double time_used;
 } parStruct;
 
 int asmfunc(short *mat, char *vec, long nr, long nc, float *dest);
@@ -37,12 +42,11 @@ lcore_math(void *arg)
 	struct timeval start_t, end_t;
 	double time_used;
 
-	lcore_id = rte_lcore_id();
 	gettimeofday(&start_t, NULL);
 	asmfunc(params->mat, params->vec, params->nr, params->nc, params->dest);
 	gettimeofday(&end_t, NULL);
 	time_used = (end_t.tv_sec - start_t.tv_sec) + (end_t.tv_usec - start_t.tv_usec) / 1000000.;
-	printf("CPU core #%u  Real time: %.3f\n", lcore_id, time_used);
+	params->time_used = time_used;
 
 	return 0;
 }
@@ -56,11 +60,12 @@ main(int argc, char **argv)
 	unsigned lcore_id;
 	char *vec;
 	short mat[16*16*2*1024];
-	float dest[16*1024] __attribute__ ((aligned (64)));
+	float *dest;
 	int i, j, k, p, ncores;
 	parStruct params[RTE_MAX_LCORE];
 	clock_t start, end;
 	double cpu_time_used;
+	double min_t[RTE_MAX_LCORE], max_t[RTE_MAX_LCORE], avg_t[RTE_MAX_LCORE];
 
 	ret = rte_eal_init(argc, argv);
 	if (ret < 0)
@@ -68,9 +73,12 @@ main(int argc, char **argv)
 	/* >8 End of initialization of Environment Abstraction Layer */
 
 	/* Create the data buffer */
-	vec = rte_malloc_socket(NULL, NR_pack*1024*16, 0x40, rte_socket_id());
+	vec = rte_malloc_socket(NULL, 1024*16*NR_pack, 0x40, rte_socket_id());
 	if (vec == NULL)
-		rte_exit(EXIT_FAILURE, "Cannot init data buffer %d\n", i);
+		rte_exit(EXIT_FAILURE, "Cannot init data buffer\n");
+	dest = rte_malloc_socket(NULL, 1024*64*NR_run, 0x40, rte_socket_id());
+	if (dest == NULL)
+		rte_exit(EXIT_FAILURE, "Cannot init sum buffer\n");
 
 	// transposed matrix
 	for (k=0; k<1024; k++)
@@ -82,34 +90,59 @@ main(int argc, char **argv)
 				mat[p + 1] = 0;
 			}
 
-	/* Launches the function on each lcore. 8< */
-	i = 1;
-	start = clock();
-	RTE_LCORE_FOREACH_WORKER(lcore_id) {
-		params[i].mat = mat + i * NR_ch * 512;
-		params[i].vec = vec + i * NR_ch * 16;
-		params[i].nr = NR_pack;
-		params[i].nc = NR_ch;
-		params[i].dest = dest + i * NR_ch * 16;
-		rte_eal_remote_launch(lcore_math, &params[i], lcore_id);
-		i++;
-		if (i >= 16) break;
+	/* performance statistic */
+	for (i=0; i<RTE_MAX_LCORE; i++) {
+		min_t[i] = DBL_MAX;
+		max_t[i] = DBL_MIN;
+		avg_t[i] = 0.0;
 	}
-	ncores = i;
 
-	/* call it on main lcore too */
-	params[0].mat = mat;
-	params[0].vec = vec;
-	params[0].nr = NR_pack;
-	params[0].nc = NR_ch;
-	params[0].dest = dest;
-	lcore_math(&params[0]);
-	/* >8 End of launching the function on each lcore. */
+	/* Launches the function on each lcore. 8< */
+	start = clock();
 
-	rte_eal_mp_wait_lcore();
+	for (j=0; j<NR_run; j++) {
+		i = 1;
+		RTE_LCORE_FOREACH_WORKER(lcore_id) {
+			params[i].mat = mat + i * NR_ch * 512;
+			params[i].vec = vec + (i * NR_ch + 1024 * NR_sum * j) * 16;
+			params[i].nr = NR_sum;
+			params[i].nc = NR_ch;
+			params[i].dest = dest + (i * NR_ch + 1024 * j) * 16;
+			rte_eal_remote_launch(lcore_math, &params[i], lcore_id);
+			i++;
+			if (i >= NR_cpu) break;
+		}
+		ncores = i;
+
+		/* call it on main lcore too */
+		params[0].mat = mat;
+		params[0].vec = vec + 1024 * 16 * NR_sum * j;
+		params[0].nr = NR_sum;
+		params[0].nc = NR_ch;
+		params[0].dest = dest + 1024 * 16 * j;
+		lcore_math(&params[0]);
+		/* >8 End of launching the function on each lcore. */
+
+		rte_eal_mp_wait_lcore();
+
+		/* performance statistic */
+		for (i=0; i<ncores; i++) {
+			float t = params[i].time_used;
+			if (min_t[i] > t) min_t[i] = t;
+			if (max_t[i] < t) max_t[i] = t;
+			avg_t[i] += t;
+		}
+	}
+
 	end = clock();
 	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-	printf("Total cores: %d  Total CPU time: %.3f\n", ncores, cpu_time_used);
+	printf("Total cores: %d  Total CPU time: %.3f  Average time: %.3f\n",
+			ncores, cpu_time_used, cpu_time_used/ncores);
+
+	for (i=0; i<ncores; i++) {
+		printf("CPU core #%2d  min:avg:max %.3f:%.3f:%.3f\n", i,
+				min_t[i]*NR_run, avg_t[i]/NR_run*NR_run, max_t[i]*NR_run);
+	}
 
 	/* clean up the EAL */
 	rte_free(vec);
