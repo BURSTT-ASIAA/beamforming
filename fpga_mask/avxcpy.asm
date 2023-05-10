4b853f43f8476de05a21b9d6c84d7b63708bac09
ChihYi Wen <wency@asiaa.sinica.edu.tw>
2023-05-10 11:33:33 +0800

    add avxcopy

---
+++ fpga_mask/avxcpy.asm
@@ -0,0 +1,33 @@
+	section .text
+
+	global avxcpy
+avxcpy:
+	push rbp
+	mov rbp, rsp
+
+	xor rax, rax
+loop:
+	vmovdqu16 zmm0, [rsi+rax]
+	vmovdqu16 [rdi+rax], zmm0
+	add rax, 64
+	cmp rax, rdx
+	jb loop
+
+	leave
+	ret
+
+	global avxfill
+avxfill:
+	push rbp
+	mov rbp, rsp
+
+	xor rax, rax
+loop2:
+	vmovdqu16 zmm0, [rsi]
+	vmovdqu16 [rdi+rax], zmm0
+	add rax, 64
+	cmp rax, rdx
+	jb loop2
+
+	leave
+	ret
