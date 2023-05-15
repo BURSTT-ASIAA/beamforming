cea996e42da0901528f538ddebf6fc39d07dd144
ChihYi Wen <wency@asiaa.sinica.edu.tw>
2023-05-15 11:11:34 +0800

    fp16...

--- fpga_mask/multiply.c
+++ fpga_mask/multiply.c
@@ -66,6 +66,42 @@ lcore_math(void *arg)
 }
 /* >8 End of launching function on lcore. */
 
+static int
+lcore_math_fp16(void *arg)
+{
+	parStruct *params = arg;
+	struct timeval start_t, end_t;
+	double time_used, min_t, max_t, avg_t;
+	char *vec;
+	char *dest;
+	int i;
+
+	min_t = DBL_MAX;
+	max_t = DBL_MIN;
+	avg_t = 0.0;
+	vec = params->vec;
+	dest = params->dest;
+	for (i=0; i<params->repeat; i++) {
+		gettimeofday(&start_t, NULL);
+		multiply_fp16(params->mat, vec, params->nr, dest);
+		gettimeofday(&end_t, NULL);
+
+		time_used = (end_t.tv_sec - start_t.tv_sec) + (end_t.tv_usec - start_t.tv_usec) / 1000000.;
+		if (min_t > time_used) min_t = time_used;
+		if (max_t < time_used) max_t = time_used;
+		avg_t += time_used;
+
+		vec += 64 * params->nr;
+		dest += 32;
+	}
+
+	params->min_t = min_t;
+	params->max_t = max_t;
+	params->avg_t = avg_t / params->repeat;
+
+	return 0;
+}
+
 /* Initialization of Environment Abstraction Layer (EAL). 8< */
 int
 main(int argc, char **argv)
@@ -110,6 +146,7 @@ main(int argc, char **argv)
 	params.repeat = NR_run;
 	params.dest = dest;
 
+	printf("\n====== VNNI ======\n");
 	for (p=0; p<NR_REPEAT; p++) {
 		/* performance statistic */
 		start = clock();
@@ -117,6 +154,28 @@ main(int argc, char **argv)
 
 		/* Launches the function */
 		lcore_math(&params);
+
+		printf("\n====== test #%d ======\n", p+1);
+		end = clock();
+		gettimeofday(&end_t, NULL);
+		cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+		time_used = (end_t.tv_sec - start_t.tv_sec) + (end_t.tv_usec - start_t.tv_usec) / 1000000.;
+		printf("Total CPU time: %.3f   Real time: %3f\n", cpu_time_used, time_used);
+
+		min_t = params.min_t;
+		max_t = params.max_t;
+		avg_t = params.avg_t;
+		printf("min:avg:max %.3f:%.3f:%.3f\n", min_t*1000, avg_t*1000, max_t*1000);
+	}
+
+	printf("\n====== FP16 ======\n");
+	for (p=0; p<NR_REPEAT; p++) {
+		/* performance statistic */
+		start = clock();
+		gettimeofday(&start_t, NULL);
+
+		/* Launches the function */
+		lcore_math_fp16(&params);
 
 		printf("\n====== test #%d ======\n", p+1);
 		end = clock();
